5c656a286b26ee52491a7dfad45d29518d45d669
vinhdevED <tdinhvinh2002@gmail.com>
2024-06-11 21:20:50 +0700

    Update HTML

--- sourceserver/server.js
+++ sourceserver/server.js
@@ -6,7 +6,7 @@ const app = express();
 const udpClient = dgram.createSocket('udp4');
 const port = 3000;
 const BROADCAST_PORT = 41234; // Cổng để gửi gói tin UDP
-const IOT_DEVICE_IP = '192.168.1.101'; // Địa chỉ broadcast để gửi gói tin tới tất cả thiết bị trong mạng cục bộ
+const IOT_DEVICE_IP = '192.168.1.69'; // Địa chỉ broadcast để gửi gói tin tới tất cả thiết bị trong mạng cục bộ
 
 // Cấu hình socket để cho phép gửi broadcast
 udpClient.bind(() => {
@@ -14,13 +14,18 @@ udpClient.bind(() => {
 });
 
 // Serve the HTML file
+
+app.use(express.static(path.join(__dirname, 'template')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'website.html'));
+    res.sendFile(path.join(__dirname, 'template', 'website.html'));
 });
 
 // Endpoint to trigger UDP broadcast to IoT devices
+
 app.get('/send-request', (req, res) => {
     const message = Buffer.from('DISCOVER_DEVICES');
+
     udpClient.send(message, 0, message.length, BROADCAST_PORT, IOT_DEVICE_IP, (err) => {
         if (err) {
             console.error('Error sending message:', err);
@@ -28,22 +33,48 @@ app.get('/send-request', (req, res) => {
         } else {
             console.log('Broadcast message sent to IoT devices');
 
-            // Set up listener for responses
-            udpClient.on('message', (msg, rinfo) => {
-                console.log(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
-                res.send(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
-            });
+            const responses = [];
 
-            // Set a timeout for waiting for responses
+            const messageHandler = (msg, rinfo) => {
+                console.log(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
+                responses.push(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
+            };
+
+            udpClient.on('message', messageHandler);
+
             setTimeout(() => {
-                udpClient.close();
-                console.log('No more responses. Closed the socket.');
+                udpClient.removeListener('message', messageHandler);
+                res.send(responses);
+                console.log('No more responses. Waiting period ended.');
             }, 5000); // Đợi 5 giây để nhận phản hồi
         }
     });
 });
+// app.get('/send-request', (req, res) => {
+//     const message = Buffer.from('DISCOVER_DEVICES');
+//     udpClient.send(message, 0, message.length, BROADCAST_PORT, IOT_DEVICE_IP, (err) => {
+//         if (err) {
+//             console.error('Error sending message:', err);
+//             res.status(500).send('Error sending message');
+//         } else {
+//             console.log('Broadcast message sent to IoT devices');
+//
+//             // Set up listener for responses
+//             udpClient.on('message', (msg, rinfo) => {
+//                 console.log(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
+//                 res.send(`Received message from ${rinfo.address}:${rinfo.port} - ${msg}`);
+//             });
+//
+//             // Set a timeout for waiting for responses
+//             setTimeout(() => {
+//                 udpClient.close();
+//                 console.log('No more responses. Closed the socket.');
+//             }, 5000); // Đợi 5 giây để nhận phản hồi
+//         }
+//     });
+// });
 
 // Start the HTTP server
 app.listen(port, () => {
-    console.log(`HTTP server running at http://192.168.1.100:${port}`);
+    console.log(`HTTP server running at http://localhost:${port}`);
 });
